e66158bb2669283a68d5cb413d42d4af84b6991c
nikzdev <nikzdev@gmail.com>
2020-08-30 21:52:04 +0530

    Href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Navbar = () => {
     return (
         <div className="ui massive inverted menu">
-           <a class="active item red">
+           <a href="/" class="active item red">
     Youtube Lite 
     <i class="video play icon"></i>
   </a>
